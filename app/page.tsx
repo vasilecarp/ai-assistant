2fbff06cda44abf3495eb7aa3145f60011af339f
vasilecarp <vasilecarpconsulting@gmail.com>
2024-03-19 16:48:01 +0200

    Add header image and configure remote patterns for images

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,16 @@ export default function Home() {
   return (
     <main className="">
       <h1>AI assistant</h1>
+      {/* Header */}
+      <header>
+        <Image
+          src="https://i.imgur.com/MCHWJZS.png"
+          alt="Logo"
+          width={50}
+          height={50}
+        />
+      </header>
+      {/* Form */}
     </main>
   );
 }
