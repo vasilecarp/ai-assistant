c4f74392b0da6e747bef3a02601a2281bfed2706
vasilecarp <vasilecarpconsulting@gmail.com>
2024-03-19 17:04:59 +0200

    Add settings icon to header

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import Image from "next/image";
+import { SettingsIcon } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="">
-      <h1>AI assistant</h1>
       {/* Header */}
-      <header>
+      <header className="flex fixed top-0 justify-between text-white w-full p-5">
         <Image
           src="https://i.imgur.com/MCHWJZS.png"
           alt="Logo"
           width={50}
           height={50}
+        />
+
+        <SettingsIcon
+          className="p-2 m-2 rounded-full cursor-pointer bg-purple-600 text-black transition-all ease-in-out duration-150 hover:bg-purple-700 hover:text-white"
+          //onClick={() => setDisplaySettings(!displaySettings)}
+          size={40}
         />
       </header>
       {/* Form */}
