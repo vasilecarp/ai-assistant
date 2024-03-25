eb5d160a0720e82498593c1322a8b5f7509492a0
vasilecarp <vasilecarpconsulting@gmail.com>
2024-03-25 12:33:11 +0200

    Refactor Recorder component to include an active assistant icon

--- components/Recorder.tsx
+++ components/Recorder.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import activeAssistantIcon from "@/img/active.gif";
+import notActiveAssistantIcon from "@/img/notactive.png";
 
 function Recorder() {
   return (
-    <div className='flex items-center justify-center'>Recorder</div>
-  )
+    <div className="flex items-center justify-center text-white">
+      <Image
+        src={activeAssistantIcon}
+        alt="Recording"
+        width={350}
+        height={350}
+        //onClick={stopRecording}
+        priority={true}
+        className="assistant grayscale"
+      />
+    </div>
+  );
 }
 
-export default Recorder
+export default Recorder;
